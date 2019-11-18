20ac39f6f9abfdb056e035660085f9d72d518c52
Egor Timashev <metallagetemp@gmail.com>
2019-11-18 16:01:24 +0300

    UPD: NUnit Test for creating SQLite DB

---
+++ ForDebug/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Data;
+using System.Data.SQLite;
+using CompMgr.Model.DBUtils;
+
+namespace ForDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fileName = "debug.db";
+            if (File.Exists(fileName))
+                File.Delete(fileName);
+
+            SQLiteDataBaseCreator creator1 = new SQLiteDataBaseCreator();
+            creator1.CreateDataBase(fileName);
+            GetTables(fileName);
+
+
+
+        }
+
+        static void GetTables(string dbPath)
+        {
+            string connectionString = $"DataSource={dbPath};Version=3;";
+            string selectTablesScript = @"SELECT * FROM sqlite_master WHERE type='table'";
+
+            using (SQLiteConnection testSqCon = new SQLiteConnection(connectionString))
+            {
+                DataTable tables = new DataTable("tables");
+                SQLiteCommand selectTables = new SQLiteCommand(testSqCon);
+                selectTables.CommandText = selectTablesScript;
+
+                try
+                {
+                    testSqCon.Open();
+
+
+                    tables.Load(selectTables.ExecuteReader());
+                }
+                finally
+                {
+                    testSqCon.Close();
+                }
+
+
+
+                foreach (DataColumn dc in tables.Columns)
+                    Console.Write("{0} ({1})\t", dc.ColumnName, dc.DataType.Name);
+                Console.Write("\n");
+
+                foreach (DataRow dr in tables.Rows)
+                    Console.WriteLine("{0}\t{1}\t{2}", dr.Field<string>("type"), dr.Field<string>("name"), dr.Field<string>("tbl_name"), dr.Field<string>("sql"));
+
+                //    Console.WriteLine(,type, name, tbl_name, rootpage(int), sql);
+                Console.WriteLine("Таблица Users {0}", tables.Select("name = 'Ololo'").Count() == 1);
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- Model_Testing/SQLiteDBCheckerTest.cs
+++ Model_Testing/SQLiteDBCheckerTest.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using NUnit.Framework;
+
 using CompMgr.Model.DBUtils;
 using System.Data.SQLite;
 
@@ -11,15 +15,78 @@ namespace Model_Testing
     public class SQLiteDBCheckerTest
     {
         SQLiteDataBaseCreator testDBCreator = new SQLiteDataBaseCreator();
+        string testFile = "test.db";
+        DataTable tables;
+
+        private void GetTables(string dbPath)
+        {
+            string connectionString = $"DataSource={dbPath};Version=3;";
+            string selectTablesScript = @"SELECT * FROM sqlite_master WHERE type='table'";
+
+            using (SQLiteConnection testSqCon = new SQLiteConnection(connectionString))
+            {
+                DataTable tables = new DataTable("tables");
+                SQLiteCommand selectTables = new SQLiteCommand(testSqCon);
+                selectTables.CommandText = selectTablesScript;
+
+                try
+                {
+                    testSqCon.Open();
+                    tables.Load(selectTables.ExecuteReader());
+                }
+                finally
+                {
+                    testSqCon.Close();
+                }
+                this.tables = tables;
+            }
+        }
 
         [Test]
-        public void TestDBCreation()
+        public void DataBaseCreationTest()
         {
-            string testFile = "test.db";
-
-
             testDBCreator.CreateDataBase(testFile);
-            Assert.IsTrue(File.Exists(testFile));
+            Assert.IsTrue(File.Exists(testFile)&&UsersTableCreationTest()&&ComputersTableCreationTest()&&DivisionsTableCreationTest()&&
+                InstallsTableCreationTest()&&SoftwaresTableCreationTest());
         }
+
+
+       
+        private bool UsersTableCreationTest()
+        {
+            if (tables == null)
+                GetTables(testFile);
+            return tables.Select("name = 'Users'").Count()==1;
+        }
+
+
+        private bool ComputersTableCreationTest()
+        {
+            if (tables == null)
+                GetTables(testFile);
+            return tables.Select("name = 'Computers'").Count() == 1;
+        }
+
+        private bool DivisionsTableCreationTest()
+        {
+            if (tables == null)
+                GetTables(testFile);
+            return tables.Select("name = 'Divisions'").Count() == 1;
+        }
+
+        private bool InstallsTableCreationTest()
+        {
+            if (tables == null)
+                GetTables(testFile);
+            return tables.Select("name = 'Installs'").Count() == 1;
+        }
+
+        private bool SoftwaresTableCreationTest()
+        {
+            if (tables == null)
+                GetTables(testFile);
+            return tables.Select("name = 'Softwares'").Count() == 1;
+        }
+
     }
 }
