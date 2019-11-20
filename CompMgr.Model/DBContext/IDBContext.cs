22f008cb4f111638739e39ac09e31bf638d233cd
Egor Timashev <metallagetemp@gmail.com>
2019-11-20 16:48:48 +0300

    Add DBContext. TODO connection string

---
+++ CompMgr.Model/DBContext/IDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using CompMgr.Model.Entities;
+
+namespace CompMgr.Model.DBContext
+{
+    interface IDBContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Software> Softwares { get; set; }
+        DbSet<Division> Divisions { get; set; }
+        DbSet<Computer> Computers { get; set; }
+        DbSet<Install> Installs { get; set; }
+
+    }
+}
---
+++ CompMgr.Model/DBContext/SQLiteDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Data;
+using CompMgr.Model.Entities;
+
+namespace CompMgr.Model.DBContext
+{
+    class SQLiteDBContext :DbContext, IDBContext
+    {
+        
+        public SQLiteDBContext():base("sqliteDataBase")
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Software> Softwares { get; set; }
+        public DbSet<Division> Divisions { get; set; }
+        public DbSet<Computer> Computers { get; set; }
+        public DbSet<Install> Installs { get; set; }
+    }
+}
